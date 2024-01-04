5c5db28a1696439202ceeb8489c7e78e1e8bdb8a
Kwangs <leekp7@naver.com>
2024-01-04 19:54:43 +0900

    LKP_0104_01 [DB, 서버 초기 세팅]

--- ReactRepo/sul/src/component/member/common/Header.jsx
+++ ReactRepo/sul/src/component/member/common/Header.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeaderDiv = styled.div`
+    width: 100%;
+    height: 56px;
+    padding: 0px 69px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    & > .header_left {
+        width:500px;
+        height: 50px;
+        display: flex;
+        box-sizing: border-box;
+    }
+    & > .header_left > :nth-child(1){
+        width:100px;
+        height: 50px;
+        display: flex;
+    }
+    & > .header_left > :
+`
 
 const Header = () => {
     return (
-        <div>
-            <h1>유저 헤더~~~</h1>
-        </div>
+        <StyledHeaderDiv>
+            <div className='header_left'>
+                <div className='icon'>icon</div>
+                <div className='search'>search</div>
+            </div>
+            <div className='header_right'>
+                <div className='join_login'>회원가입/로그인</div>
+                <div className='cart'>장바구니</div>
+            </div>
+        </StyledHeaderDiv>
     );
 };
 
