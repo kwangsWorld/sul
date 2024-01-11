4910aea106f835fe095f7ca0a6379f9132e773b1
Kwangs <leekp7@naver.com>
2024-01-11 17:56:07 +0900

    백엔드: 검색기능 추가

--- SpringRepo/sulPrj/src/main/java/com/sul/app/product/controller/ProductController.java
+++ SpringRepo/sulPrj/src/main/java/com/sul/app/product/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sul.app.product.service.ProductService;
@@ -25,17 +27,24 @@ public class ProductController {
 		return voList;
 	}
 	
-	@GetMapping("")
 	//제품 카테고리 별 전체 조회
-	public List<ProductVo> listByCategory(ProductVo vo){
+	@GetMapping("{vo}")
+	public List<ProductVo> listByCategory(@PathVariable("vo") ProductVo vo){
 		List<ProductVo> voList = service.listByCategory(vo);
 		return voList;
 	}
 	
-	@GetMapping("detail")
 	//제품 상세 조회
+	@GetMapping("detail")
 	public ProductVo detail(ProductVo vo) {
 		ProductVo productVo = service.detail(vo);
 		return productVo;
+	}
+	
+	//제품 검색
+	@GetMapping("search")
+	public List<ProductVo> search(ProductVo vo) {
+		List<ProductVo> voList = service.searchByName(vo);
+		return voList;
 	}
 }
