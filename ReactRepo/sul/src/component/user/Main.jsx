bedb07c228070ab09f721d536327d20324024dad
zwoo1574 <cherry1574@naver.com>
2024-01-12 18:38:23 +0900

    [Main.jsx]

--- ReactRepo/sul/src/component/user/Main.jsx
+++ ReactRepo/sul/src/component/user/Main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import UserMain from './member/UserMain';
 import MypageMain from './mypage/MypageMain';
 import ComMain from './community/ComMain';
+import MemberMain from './member/MemberMain';
 import styled from 'styled-components';
 import ProductMain from './product/ProductMain';
 
@@ -18,7 +18,7 @@ const Main = () => {
     return (
         <StyledMainDiv>
             <Routes>
-                <Route path='/member/*' element={<UserMain />}></Route>
+                <Route path='/member/*' element={<MemberMain />}></Route>
                 <Route path='/mypage/*' element={<MypageMain />}></Route>
                 <Route path='/community/*' element={<ComMain />}></Route>
                 <Route path='/product/*' element={<ProductMain />}></Route>
