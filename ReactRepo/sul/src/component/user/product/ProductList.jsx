b9b8aecdcbc748a23a72efa30245151eefcd6797
Kwangs <leekp7@naver.com>
2024-01-15 18:01:26 +0900

    메인화면 List 데이터 연결 완료

--- ReactRepo/sul/src/component/user/product/ProductList.jsx
+++ ReactRepo/sul/src/component/user/product/ProductList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ProductListItem from './ProductListItem';
 
@@ -16,21 +16,34 @@ const StyledListDiv = styled.div`
     .second_text{
         grid-column: span 5;
     }
-
-    
 `;
 
+
 const ProductList = () => {
+
+    const [arr, setArr] = useState([]);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8888/app/product/list",
+        {method : 'get'})
+        .then( (resp) => {return resp.json()})
+        .then( (voList) => {return setArr(voList);
+        } )
+        ;
+    } , [] );
+
+
     return (
+
         <StyledListDiv>
             <div className='first_text'> 
             청룡의 해 시작을 함께할 우리술!
             </div>
-            <ProductListItem/>
-            <ProductListItem/>
-            <ProductListItem/>
-            <ProductListItem/>
-            <ProductListItem/>
+           {
+                arr.map( (vo) => {
+                    return <ProductListItem a={vo.pName} b={vo.price} c={vo.rating} no={vo.productNo}/>;
+            } )
+            }
             <div className='second_text'>
             평점이 증명해주는 Hot of Hot한 우리술!
             </div>
--- SpringRepo/sulPrj/src/main/java/com/sul/app/product/controller/ProductController.java
+++ SpringRepo/sulPrj/src/main/java/com/sul/app/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +25,8 @@ public class ProductController {
 	//제품 전체 조회
 	@GetMapping("list")
 	public List<ProductVo> list(ProductVo vo){
-		System.out.println("되나?111");
 		List<ProductVo> voList = service.list(vo);
-		System.out.println("되나?222");
+		System.out.println(voList);
 		return voList;
 	}
 	
